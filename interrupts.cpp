77e72a2461fb87610675e7e7db1a7d91bab8bdd4
GyuoMi <darshnsingh10@gmail.com>
2023-11-30 20:53:25 +0200

    keyboard interrupts slave and master comm with pic

--- interrupts.cpp
+++ interrupts.cpp
@@ -2,7 +2,27 @@
 
 void printf(char* str);
 
+
+InterruptHandler::InterruptHandler(uint8_t interruptNumber,InterruptManager* interruptManager)
+{
+    this->interruptNumber = interruptNumber;
+    this->interruptManager = interruptManager;
+    interruptManager->handlers[interruptNumber] = this;
+}
+InterruptHandler::~InterruptHandler()
+{
+    if(interruptManager->handlers[interruptNumber] == this)
+        interruptManager->handlers[interruptNumber] = 0;
+
+}
+uint32_t InterruptHandler::HandleInterrupt(uint32_t esp)
+{
+    return esp;
+}
+
+
 InterruptManager::GateDescriptor InterruptManager::interruptDescriptorTable[256];
+InterruptManager* InterruptManager::ActiveInterruptManager = 0;
 
 void InterruptManager::SetInterruptDescriptorTableEntry(
 	uint8_t interruptNumber,
@@ -34,8 +54,10 @@ InterruptManager::InterruptManager(GlobalDescriptorTable *gdt)
 	const uint8_t IDT_INTERRUPT_GATE = 0xE;
 
 	for (uint16_t i = 0; i < 256; i++)
+    {
+        handlers[i] = 0;
 		SetInterruptDescriptorTableEntry(i, CodeSegment, &IgnoreInterruptRequest, 0, IDT_INTERRUPT_GATE);
-
+    }
 
 	SetInterruptDescriptorTableEntry(0x20, CodeSegment, &HandleInterruptRequest0x00, 0, IDT_INTERRUPT_GATE);
 	/* getting an interrupt 21 will jump to interrupts.s HandleInterrupt function with 0x20 (IRQ) + 0x01 (num)
@@ -77,11 +99,49 @@ void InterruptManager::Activate()
 	// start interrupts, kernel has idt (interrupt manager) so we first instantiate it
 	// thereafter we instatiate all hardware (need idt so hardware can tell interrupt manager whichever interrupts to handle)
 	// lastly we activate the interrupts
+	if(ActiveInterruptManager != 0)
+		ActiveInterruptManager->Deactivate();
+	ActiveInterruptManager = this;
 	asm("sti");
+}
+void InterruptManager::Deactivate()
+{
+
+	if(ActiveInterruptManager == this)
+	{
+		ActiveInterruptManager = 0;
+		asm("cli");
+	}
 }
 
 uint32_t InterruptManager::handleInterrupt(uint8_t interruptNumber, uint32_t esp)
 {	
-	printf(" INTERRUPT");
+	if(ActiveInterruptManager != 0)
+		ActiveInterruptManager->DoHandleInterrupt(interruptNumber, esp);
+	return esp;
+}
+
+uint32_t InterruptManager::DoHandleInterrupt(uint8_t interruptNumber, uint32_t esp)
+{
+    if(handlers[interruptNumber] != 0)
+    {
+        esp = handlers[interruptNumber]->HandleInterrupt(esp);
+    }
+    else if(interruptNumber != 0x20)
+    {    
+        char* foo = "UHNHANDLED  INTERRUPT 0x00";
+        char* hex = "0123456789ABCDEF";
+        foo[22] = hex[(interruptNumber >> 4) & 0x0F];
+        foo[23] = hex[interruptNumber & 0x0F];
+        printf(foo);
+    }
+
+    //we only need to send answers to hardware interrupts
+    if(0x20 <= interruptNumber && interruptNumber < 0x30)
+    {
+        picMasterCommand.Write(0x20);
+    if(0x28 <= interruptNumber )
+        picSlaveCommand.Write(0x20);
+    }
 	return esp;
 }
--- interrupts.h
+++ interrupts.h
@@ -5,36 +5,56 @@
 #include "port.h"
 #include "gdt.h"
 
+
+class InterruptManager;
+
+class InterruptHandler
+{
+    protected:
+        uint8_t interruptNumber; 
+        InterruptManager* interruptManager;
+
+        InterruptHandler(uint8_t interruptNumber,InterruptManager* interruptManager);
+        ~InterruptHandler();
+    public:
+        uint32_t HandleInterrupt(uint32_t esp);
+};
+
 class InterruptManager
 {
+    friend class InterruptHandler;
+	//we have to have a static pointer to the interrupt manager since we're working in a static environment
 	protected:
-	struct GateDescriptor
-	{
-		uint16_t handlerAddressLowBits;
-		uint16_t gdt_codeSegmentSelector;
-		uint8_t reserved;
-		uint8_t access;
-		uint16_t handlerAddressHighBits;
-	}__attribute__((packed));
-/*When packed attribute is applied to a struct or class, it informs the compiler to minimize the 
-padding between members, ensuring that each member is packed tightly without
- introducing any padding bytes.*/
-	struct InterruptDescriptorTablePointer
-	{
-		uint16_t size;
-		uint32_t base;
-	}__attribute__((packed));
+	    static InterruptManager* ActiveInterruptManager;
+        InterruptHandler* handlers[256];
 
-	static GateDescriptor interruptDescriptorTable[256];
+    	struct GateDescriptor
+	    {
+		    uint16_t handlerAddressLowBits;
+		    uint16_t gdt_codeSegmentSelector;
+		    uint8_t reserved;
+		    uint8_t access;
+		    uint16_t handlerAddressHighBits;
+	    }__attribute__((packed));
+    /*When packed attribute is applied to a struct or class, it informs the compiler to minimize the 
+    padding between members, ensuring that each member is packed tightly without
+    introducing any padding bytes.*/
+	    struct InterruptDescriptorTablePointer
+	    {
+		    uint16_t size;
+		    uint32_t base;
+	    }__attribute__((packed));
 
-	static void SetInterruptDescriptorTableEntry(
-		uint8_t interruptNumber,
-		uint16_t gdt_codeSegmentSelectorOffset,
-		void (*handler)(),
-		// access rights
-		uint8_t DescriptorPrivelegeLevel,
-		uint8_t DescriptorType
-	);
+	    static GateDescriptor interruptDescriptorTable[256];
+
+	    static void SetInterruptDescriptorTableEntry(
+		    uint8_t interruptNumber,
+	    	uint16_t gdt_codeSegmentSelectorOffset,
+		    void (*handler)(),
+		    // access rights
+		    uint8_t DescriptorPrivelegeLevel,
+		    uint8_t DescriptorType
+	    );
 
 
 	//ports for communication with PIC (Master & Slave)
@@ -49,8 +69,11 @@ padding between members, ensuring that each member is packed tightly without
 		~InterruptManager();
 
 		void Activate();
+		void Deactivate();
 
 		static uint32_t handleInterrupt(uint8_t interruptNumber, uint32_t esp);
+
+		uint32_t DoHandleInterrupt(uint8_t interruptNumber, uint32_t esp);
 
 		static void IgnoreInterruptRequest();
 		// timer interrupt
